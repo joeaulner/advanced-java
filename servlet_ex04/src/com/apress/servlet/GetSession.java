2121fc72822eecf6122a4a578c8969238b77f83f
Joe Aulner <joseph.aulner@aviture.us.com>
2017-03-11 15:52:57 -0600

    Completed Servlet example 04

---
+++ servlet_ex04/src/com/apress/servlet/GetSession.java
@@ -0,0 +1,40 @@
+package com.apress.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class GetSession extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        HttpSession session = request.getSession(false);
+        try {
+            response.setContentType("text/html");
+            PrintWriter writer = response.getWriter();
+            writer.println("<html><body>");
+            if (session == null) {
+                writer.println("You are not logged in");
+            } else {
+                writer.println("Thank you, you are already logged in");
+                writer.println("Here is the data in your session");
+                Enumeration names = session.getAttributeNames();
+                while (names.hasMoreElements()) {
+                    String name = (String) names.nextElement();
+                    Object value = session.getAttribute(name);
+                    writer.println("<p>name=" + name + " value=" + value);
+                }
+            }
+            writer.println("<p><a href=\"login.html\">Return</a> to login page");
+            writer.println("</body></html>");
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ servlet_ex04/src/com/apress/servlet/Login.java
@@ -0,0 +1,35 @@
+package com.apress.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute("username", username);
+        session.setAttribute("password", password);
+
+        try {
+            response.setContentType("text/html");
+            PrintWriter writer = response.getWriter();
+            writer.println("<html><body>");
+            writer.println("Thank you, " + username + ". You are now logged into the system");
+            String newURL = response.encodeURL("GetSession");
+            writer.println("<p>Click <a href=\"" + newURL + "\">here</a> for another servlet</p>");
+            writer.println("</body></html>");
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
